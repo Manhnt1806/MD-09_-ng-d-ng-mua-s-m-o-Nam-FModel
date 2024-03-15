db2ba3cd98786e53cd7256a6d608064e0abd67f3
ManhntPH29583 <nmanh6853@gmail.com>
2024-03-15 21:57:57 +0700

    MD9_DATN_FModel

---
+++ app/src/main/java/poly/manhnt/md9_datn_fmodel/MainActivity.java
@@ -0,0 +1,14 @@
+package poly.manhnt.md9_datn_fmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
