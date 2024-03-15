81ecf953a76151984b84da834d2dff5be158fe6c
ThuHuyenn <127474306+thuhuyenn@users.noreply.github.com>
2024-03-16 00:02:34 +0700

    PlashScreen_HuyenPH29290

---
+++ app/src/main/java/poly/manhnt/md9_datn_fmodel/Views/HomeScreen/HomeActivity.java
@@ -0,0 +1,26 @@
+package poly.manhnt.md9_datn_fmodel.Views.HomeScreen;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import poly.manhnt.md9_datn_fmodel.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/poly/manhnt/md9_datn_fmodel/Views/PlashScreen/PlashAcivity.java
@@ -0,0 +1,33 @@
+package poly.manhnt.md9_datn_fmodel.Views.PlashScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import poly.manhnt.md9_datn_fmodel.R;
+import poly.manhnt.md9_datn_fmodel.Views.HomeScreen.HomeActivity;
+
+public class PlashAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plash);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                }catch (Exception e){
+
+                }finally {
+                    Intent intent = new Intent(PlashAcivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        thread.start();
+    }
+}
