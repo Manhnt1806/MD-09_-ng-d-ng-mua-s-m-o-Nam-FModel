831198b1eaa00472a49930bcc670b570c0c00b4b
hoanganhtu2203 <105504200+hoanganhtu2203@users.noreply.github.com>
2024-03-23 01:20:33 +0700

    NotifiScreen_TuhaPH19750

---
+++ app/src/main/java/poly/manhnt/md9_datn_fmodel/Views/NotifiScreen/NotifiActivity.java
@@ -0,0 +1,21 @@
+package poly.manhnt.md9_datn_fmodel.Views.NotifiScreen;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import poly.manhnt.md9_datn_fmodel.R;
+
+public class NotifiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifi);
+
+    }
+}
