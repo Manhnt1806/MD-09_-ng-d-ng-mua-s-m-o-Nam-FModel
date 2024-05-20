fc6a5beff3a122c2e1460623e30620c9fd9416f6
tientmph29266 <127921953+tientmph29266@users.noreply.github.com>
2024-05-20 23:51:07 +0700

    Expand_TientmPH29266

---
+++ app/src/main/java/poly/manhnt/md9_datn_fmodel/Models/Objects/ILoadMore.java
@@ -0,0 +1,32 @@
+package poly.manhnt.md9_datn_fmodel.Models.Objects;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ILoadMore extends RecyclerView.OnScrollListener {
+    int itemAnDauTien = 0;
+    int tongItem = 0;
+    int itemLoadTruoc = 10;
+    RecyclerView.LayoutManager layoutManager;
+    public ILoadMore(RecyclerView.LayoutManager layoutManager){
+        this.layoutManager = layoutManager;
+    }
+    @Override
+    public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+        tongItem = layoutManager.getItemCount();
+        itemAnDauTien = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+        if(tongItem <= (itemAnDauTien + itemLoadTruoc)){
+            Log.d("zzzz", tongItem +" "+ itemAnDauTien);
+        }
+
+    }
+
+    @Override
+    public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+    }
+}
