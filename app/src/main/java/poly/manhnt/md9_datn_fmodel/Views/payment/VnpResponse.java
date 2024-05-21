e55139fd82adee456d2679e48ad225b64a40cac9
tientmph29266 <127921953+tientmph29266@users.noreply.github.com>
2024-05-21 21:01:40 +0700

    fix_PayScreen_TientmPH29266

---
+++ app/src/main/java/poly/manhnt/md9_datn_fmodel/Views/payment/VnpResponse.java
@@ -0,0 +1,7 @@
+package poly.manhnt.md9_datn_fmodel.Views.payment;
+
+import com.google.gson.annotations.SerializedName;
+
+public class VnpResponse {
+    @SerializedName("vnpUrl") public String paymentUrl;
+}
